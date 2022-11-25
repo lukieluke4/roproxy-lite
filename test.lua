cc782a009db575aa9c91c1fde59617a6e8e4ec0f
lukieluke4 <46801912+lukieluke4@users.noreply.github.com>
2022-11-25 10:26:46 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -7,7 +7,7 @@ while not pizza_found do
         fireclickdetector(game.Workspace.Room.Enemies.StartButton.ClickDetector)
         wait(5)
         for i,v in pairs(game:service"Workspace".GenValues:GetDescendants()) do
-            has_word = v.Name:find("izza", 1, true)
+            has_word = v.Name:find("Luck", 1, true)
             if has_word == nil then
                 print()
             else
