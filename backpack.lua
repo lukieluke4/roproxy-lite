1040b20cc73658bfb5241f96fed69496d7a7bffb
lukieluke4 <46801912+lukieluke4@users.noreply.github.com>
2022-11-30 14:26:57 -0500

    Create backpack.lua

---
+++ backpack.lua
@@ -0,0 +1,29 @@
+syn.queue_on_teleport([[
+  repeat wait() until game:IsLoaded() and game.Players.LocalPlayer.Character
+  Wait(1)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/lukieluke4/roproxy-lite/main/tools.lua"))()
+]])
+
+local Backpack = game.Players.LocalPlayer:FindFirstChildOfClass("Backpack")
+local ammount = 10
+local delay_ = 0.01
+wait(12)
+
+while true do 
+for _, v in ipairs(Backpack:GetChildren()) do
+	v.Parent = game.Players.LocalPlayer.Character
+    if v.Name == "Capacitor" then 
+        print("no")
+    elseif v.Name == "Upgrade Module" then
+        print("no")
+    else
+    	for _ = 1, ammount do
+    		v:Activate()
+    		if delay_ then
+    			wait(delay_)
+    		end
+    	end
+	end
+	v.Parent = Backpack
+end
+end
